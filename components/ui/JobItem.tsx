be253f38d6ea937de89e0b8d23abf164f1da503a
Mohammed Atef <atefmohammed@std.mans.edu.eg>
2024-07-18 08:06:31 +0300

    add: posted time

--- components/ui/JobItem.tsx
+++ components/ui/JobItem.tsx
@@ -15,6 +15,7 @@ export interface Job {
 
 interface Detected_Extentions {
   schedule_type: string;
+  posted_at?: string;
 }
 
 interface JobItemProps {
@@ -22,7 +23,7 @@ interface JobItemProps {
 }
 
 const JobItem: React.FC<JobItemProps> = ({ job }) => {
-  const { title, company_name, location, description, detected_extensions, thumbnail, job_id } = job;
+  const { title, company_name, location, description, detected_extensions, thumbnail } = job;
   return (
     <div className="bg-white rounded-[4px] font-[roboto] p-3 grid grid-cols-[max-content_1fr] grid-rows-[max-content_max-content] border border-transparent lg:grid-cols-[max-content_1fr_max-content_max-content] lg:grid-rows-1 shadow-sm hover:border-blue-700 transition-colors">
       <Image
@@ -60,7 +61,7 @@ const JobItem: React.FC<JobItemProps> = ({ job }) => {
             fill="#B7BCCE">
             <path d="m614-310 51-51-149-149v-210h-72v240l170 170ZM480-96q-79.38 0-149.19-30T208.5-208.5Q156-261 126-330.96t-30-149.5Q96-560 126-630q30-70 82.5-122t122.46-82q69.96-30 149.5-30t149.55 30.24q70 30.24 121.79 82.08 51.78 51.84 81.99 121.92Q864-559.68 864-480q0 79.38-30 149.19T752-208.5Q700-156 629.87-126T480-96Zm0-384Zm.48 312q129.47 0 220.5-91.5Q792-351 792-480.48q0-129.47-91.02-220.5Q609.95-792 480.48-792 351-792 259.5-700.98 168-609.95 168-480.48 168-351 259.5-259.5T480.48-168Z" />
           </svg>
-          <p className="font-[500] text-xs">1 day ago</p>
+          <p className="font-[500] text-xs">{detected_extensions?.posted_at ? detected_extensions.posted_at : "unknown time"}</p>
         </div>
       </div>
     </div>
